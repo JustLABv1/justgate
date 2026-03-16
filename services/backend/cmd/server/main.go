2171bf95c40adb198ec596c80c6179e6baa88a4a
Justin Neubert <neubert_justin@outlook.com>
2026-03-16 22:03:27 +0100

    chore: bump version to 1.2.0-beta.5 in Chart.yaml, main.go, and package.json

--- services/backend/cmd/server/main.go
+++ services/backend/cmd/server/main.go
@@ -25,7 +25,7 @@ func main() {
 	databaseURL := os.Getenv("JUST_GATE_DATABASE_URL")
 
 	svc, err := service.New(service.Config{
-		Version:                   "1.2.0-beta.4",
+		Version:                   "1.2.0-beta.5",
 		AdminJWTSecret:            adminJWTSecret,
 		DatabaseURL:               databaseURL,
 		TenantHeaderName:          tenantHeaderName,
